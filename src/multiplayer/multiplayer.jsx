334853704d86ab9a2248ed39199b90afa4dfe71b
adam-clark-git <adam.b.clark@icloud.com>
2026-02-05 11:40:25 -0700

    created multiplayer.jsx

--- src/multiplayer/multiplayer.jsx
+++ src/multiplayer/multiplayer.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 export function Multiplayer() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Multiplayer displayed here</div>
-    </main>
+    <main id="drawfriends">
+            <h2> Multiplayer </h2>
+            <div className="display-logged-out">
+                Please log in to play with friends.
+            </div>
+            <div>
+                <form>
+                    <label for="text">Input Code: </label>
+                    <input type="text" id="code-input" name="varCode"/>
+                    <button id="join-lobby" className="btn btn-outline-primary my-button"> Join </button>
+                </form>
+                <br></br>
+                <p> Or </p>
+                <button id="create-lobby" className="btn btn-outline-primary my-button"> Create Lobby</button>
+                <ul>
+                    <li>
+                        <p>username1</p>
+                        <button className="kick btn btn-outline-primary my-button"> Kick</button>
+                    </li>
+                    <li>
+                        <p>username2</p>
+                        <button className="kick btn btn-outline-primary my-button"> Kick</button>
+                    </li>
+                </ul>
+                <a id="start-multiplayer" href="daily.html" className="btn btn-primary my-button" role="button"> Start the Game</a>
+            </div>
+        </main>
   );
 }
