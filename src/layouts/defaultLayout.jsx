3b77fa8e4594fabfcfeb45ac9e87c27d586272a7
adam-clark-git <adam.b.clark@icloud.com>
2026-02-05 12:33:03 -0700

    fixed default layout links

--- src/layouts/defaultLayout.jsx
+++ src/layouts/defaultLayout.jsx
@@ -7,18 +7,18 @@ export function DefaultLayout ({ children }) {
         <div className="body">
             <header>
                 <nav>
-                    <NavLink to='' className="no-decoration">
+                    <NavLink to='/' className="no-decoration">
                         <h1 className="playwrite-nz-basic-reg logo"> 
                             Drawdle 
                         </h1>
                     </NavLink>
                     <hr></hr>
-                    <NavLink to='gallery' className="playwrite-nz-basic-reg nav-item no-decoration">Gallery</NavLink>
+                    <NavLink to='/gallery' className="playwrite-nz-basic-reg nav-item no-decoration">Gallery</NavLink>
                 </nav>
                 <div className="log-in-container">
                     <form>
                         <p className="hide-vertical"> Create an account to store and share your art with others! </p>
-                        <NavLink className="btn btn-outline-primary my-button" role="button" to='login'> Log In </NavLink>
+                        <NavLink className="btn btn-outline-primary my-button" role="button" to='/login'> Log In </NavLink>
                     </form>           
                     <div id="display-logged-in">
                         <p id="confirmation-login"> Email: sample@gmail.com</p>
@@ -29,7 +29,7 @@ export function DefaultLayout ({ children }) {
             </header>
             {children}
             <footer>
-                <NavLink className="no-decoration back-up-login btn btn-outline-primary my-button" to='login'> Log In </NavLink>
+                <NavLink className="no-decoration back-up-login btn btn-outline-primary my-button" to='/login'> Log In </NavLink>
                 <a className="no-decoration" id="githublink" href="https://github.com/adam-clark-git/web-startup"> Adam Clark Github</a>
             </footer>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
