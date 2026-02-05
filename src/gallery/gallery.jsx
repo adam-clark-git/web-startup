31724becc263565142c822bb9fa26db156bb3174
adam-clark-git <adam.b.clark@icloud.com>
2026-02-05 11:38:08 -0700

    imported gallery into jsx

--- src/gallery/gallery.jsx
+++ src/gallery/gallery.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 export function Gallery() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Gallery displayed here</div>
+    <main id="gallery">
+        <h2> Gallery </h2>
+        <ul>
+            <li>
+                <h3 className="date"> December 20th, 2025</h3>
+                <img className="gallery-image" src="images/ducksample.jpg" width="200px"></img>
+                <div className="prompt">Prompt: Pond </div>
+            </li>
+        </ul>
+        <button className="display-more btn btn-outline-primary my-button"> Show More</button>
+        <div className="display-logged-out">
+            Please log in to save and view your gallery
+        </div>
+        <div id="no-more-pieces">
+            End Of List
+        </div>
+        <div id="no-total-pieces"> 
+            Try the drawdle for today to add your first piece to your gallery!
+        </div>
     </main>
   );
 }
