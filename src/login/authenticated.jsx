6a9e42166b873501fd439eb524a0c1b57ffb999d
adam-clark-git <adam.b.clark@icloud.com>
2026-02-22 14:31:44 -0700

    moved authenticated and unauthenticared to their own files.

---
+++ src/login/authenticated.jsx
@@ -0,0 +1,8 @@
+export function Authenticated() {
+    return (
+        <div id="display-logged-in">
+            <p id="confirmation-login"> Email: sample@gmail.com</p>
+            <button id="log-out"  type="button" className="btn btn-outline-primary"> Log Out</button>
+        </div>
+    )
+}
