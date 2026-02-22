303b766ae4e6a3f80888f773d45d7da8b834194a
adam-clark-git <adam.b.clark@icloud.com>
2026-02-22 14:40:53 -0700

    changed login to reflect new changes to structure

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,46 +1,23 @@
 import React from 'react';
 import { DefaultLayout } from '../layouts/defaultLayout';
-export function Login() {
+import { UnAuthenticated } from './unauthenticated';
+import { AuthState } from './authState';
+export function Login({username, authState, onAuthChange}) {
   return (
     <DefaultLayout>
     <main className="login-page">
-        <form>
-            <p> Log in to store and share your art with others! </p>
-            <ul>
-                <li>
-                    <label for="text">Email:</label>
-                    <input type="email" id="email" name="varEmail"/>
-                </li>
-                <li>
-                    <label for="text">Password:</label>
-                    <input type="password" id="password" name="varPassword"/>
-                </li>
-            </ul>
-            <button id="login"  type="button" className="btn btn-outline-primary my-button"> Log In </button>
-            <p> Don't have an account?</p>
-            <ul>
-                <li>
-                    <label for="text">Username:</label>
-                    <input type="text" id="name" name="varName"/>
-                </li>
-                <li>
-                    <label for="text">Email:</label>
-                    <input type="email" id="email" name="varEmail"/>
-                </li>
-                <li>
-                    <label for="text">Password:</label>
-                    <input type="password" id="password" name="varPassword"/>
-                </li>
-            </ul>
-            <button id="sign-up"  type="button" className="btn btn-outline-primary my-button"> Sign Up </button>
-        </form>
-            
+        {authState === AuthState.UnAuthenticated && (
+            <UnAuthenticated
+            userName = {username}
+            onLogin={(loginUserName) => {
+                onAuthChange(loginUserName, AuthState.Authenticated)
+            }}
+            />
+        )}
+
         
         {/* Will not exist at the same time as the log in */}
-        <div id="display-logged-in">
-            <p id="confirmation-login"> Email: sample@gmail.com</p>
-            <button id="log-out"  type="button" className="btn btn-outline-primary"> Log Out</button>
-        </div>
+        
         
     </main>
     </DefaultLayout>
