de52dd6e94061c9f52d9c49eb86f6cabf23f9e9f
adam-clark-git <adam.b.clark@icloud.com>
2026-02-22 15:02:47 -0700

    removed the bootstrap call at the bottom of the html (may need to add an import statement later)

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -147,7 +147,6 @@ export function Game() {
         <footer>
             <a className="no-decoration" id="githublink" href="https://github.com/adam-clark-git/web-startup"> Adam Clark Github</a>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </div>
   );
 }
--- src/layouts/defaultLayout.jsx
+++ src/layouts/defaultLayout.jsx
@@ -32,7 +32,6 @@ export function DefaultLayout ({ children }) {
                 <NavLink className="no-decoration back-up-login btn btn-outline-primary my-button" to='/login'> Log In </NavLink>
                 <a className="no-decoration" id="githublink" href="https://github.com/adam-clark-git/web-startup"> Adam Clark Github</a>
             </footer>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         </div>
     );
 }
