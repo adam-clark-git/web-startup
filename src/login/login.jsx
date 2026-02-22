4cde928d3e861e1713c414577fb86660671a5a7c
adam-clark-git <adam.b.clark@icloud.com>
2026-02-22 15:14:03 -0700

    login now includes authenticated

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { DefaultLayout } from '../layouts/defaultLayout';
-import { UnAuthenticated } from './unauthenticated';
+import { Unauthenticated } from './unauthenticated';
 import { AuthState } from './authState';
+import { Authenticated } from './authenticated';
 export function Login({username, authState, onAuthChange}) {
   return (
     <DefaultLayout>
     <main className="login-page">
-        {authState === AuthState.UnAuthenticated && (
-            <UnAuthenticated
+        {authState === AuthState.Unknown && <h1>Login Loading...</h1>}
+        {authState === AuthState.Unauthenticated && (
+            <Unauthenticated
             userName = {username}
             onLogin={(loginUserName) => {
-                onAuthChange(loginUserName, AuthState.Authenticated)
+              onAuthChange(loginUserName, AuthState.Authenticated)
             }}
+            />
+        )}
+        {authState === AuthState.Authenticated && (
+            <Authenticated 
+            userName={username} 
+            onLogout={() => onAuthChange(username, AuthState.Unauthenticated)} 
             />
         )}
 
