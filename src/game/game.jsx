9565ae1b117d3ee1552bf8923b4abbe64848f8a0
adam-clark-git <adam.b.clark@icloud.com>
2026-02-23 00:05:30 -0700

    Fixed color Selection.

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
-import { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./game.css";
 import "../app.css";
 export function Game() {
+    /* Modals (boring) */
     const [infoModal, setInfoModal] = useState(false);
     const [finishedModal, setFinishedModal] = useState(false);
     const [waitingModal, setWaitingModal] = useState(false);
     const [ratingModal, setRatingModal] = useState(false);
     const [masterpieceModal, setMasterPieceModal] = useState(false);
+
+    /* Real Stuff */
+    const [brushColor, changeColor] = useState("#000000")
+    function onColorChange(e)
+    {
+        changeColor(e.target.value)
+    }
   return (
     <div className="body">
         <header id="game-header">
             <Button className="btn-outline-primary my-button info-button" onClick={() => setInfoModal(true)}> Info </Button>
             <div className="timer"> 1:30</div>
         </header>
+
         <main id="game">
             <div id="game-content">
                 <div className="row-align">
-                    <div id="canvas"></div>
+                    <canvas></canvas>
                     <div id="color-bar">
                         <div className="color-grid">
-                            <div className="color-group" style={{backgroundColor: '#ffffff'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#6e6e6e'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#000000'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#48bfea'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#eb4949'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#49eb71'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#ebe949'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#4e49eb'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#d149eb'}}> </div>
-                            <div className="color-group" style={{backgroundColor: '#ebbb49'}}> </div>
+                            <Button className="color-group" onClick={() => changeColor("#ffffff")} style={{backgroundColor: "#ffffff"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#6e6e6e")} style={{backgroundColor: "#6e6e6e"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#000000")} style={{backgroundColor: "#000000"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#48bfea")} style={{backgroundColor: "#48bfea"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#eb4949")} style={{backgroundColor: "#eb4949"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#49eb71")} style={{backgroundColor: "#49eb71"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#ebe949")} style={{backgroundColor: "#ebe949"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#4e49eb")} style={{backgroundColor: "#4e49eb"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#d149eb")} style={{backgroundColor: "#d149eb"}}> </Button>
+                            <Button className="color-group" onClick={() => changeColor("#ebbb49")} style={{backgroundColor: "#ebbb49"}}> </Button>
                         </div>
-                        <input type="color" name="varColor" id="color-selector"/>
+                        <input type="color" name="varColor" id="color-selector" value={brushColor} onChange={onColorChange}/>
                     </div>
                 </div>
                 <div id="brush-slider">
@@ -41,9 +50,16 @@ export function Game() {
                     <input type="range" name="varBrush" id="brush-range" min="5" max="100" step="1" value="0" />
                 </div>
             </div>
-            <p> Prompt: <h4 id="prompt"> "Springtime"</h4> </p>
+            <p> Prompt: </p><h4 id="prompt"> "Springtime"</h4>
             <Button className="btn btn-primary my-button" id="submit-art" onClick={() => setFinishedModal(true)}> Finished </Button>
         
+
+
+
+
+
+
+
             {/* Modals */}
             <Modal show={infoModal} onHide={() => setInfoModal(false)}>
                 <Modal.Header closeButton>
@@ -51,10 +67,10 @@ export function Game() {
                 </Modal.Header>
                 <Modal.Body>
                     <p>
-                    Haven't played Drawdle before? Here are the rules. <br /> <br />
+                    Haven"t played Drawdle before? Here are the rules. <br /> <br />
                     You have 90 seconds to draw a picture from scratch, trying to interpret the prompt as best you can. <br />
-                    The tools and time are very limiting, so don't worry about drawing something that doesn't look great. <br />
-                    Try to stretch your creativity, and don't stress out about it! <br/>
+                    The tools and time are very limiting, so don"t worry about drawing something that doesn"t look great. <br />
+                    Try to stretch your creativity, and don"t stress out about it! <br/>
                     <Link to="">Return to Home</Link>
                     </p>
                 </Modal.Body>
@@ -70,8 +86,8 @@ export function Game() {
                     <img alt="Your Art" src="images/ducksample.jpg" width="200px"/>
                     <div className="prompt"> "Springtime"</div>
                     <p>
-                        You made a piece of art! If you're logged in, it will automatically be saved, and you can even upload it to the web.
-                        While you're here, why don't you give a thumbs up to some other users art!
+                        You made a piece of art! If you"re logged in, it will automatically be saved, and you can even upload it to the web.
+                        While you"re here, why don"t you give a thumbs up to some other users art!
                     </p>
                     <div className="prompt"> "Springtime"</div>
                     <div className="art-selection">
@@ -144,6 +160,7 @@ export function Game() {
                 </Modal.Footer>
             </Modal>
         </main>
+
         <footer>
             <a className="no-decoration" id="githublink" href="https://github.com/adam-clark-git/web-startup"> Adam Clark Github</a>
         </footer>
