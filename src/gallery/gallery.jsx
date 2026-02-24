b3be49ffaff3ccf61a091952b87e45c3dbc1fda1
adam-clark-git <adam.b.clark@icloud.com>
2026-02-23 19:40:25 -0700

    tabbed up

--- src/gallery/gallery.jsx
+++ src/gallery/gallery.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { DefaultLayout } from '../layouts/defaultLayout';
+import { AuthState } from './authState';
+import { Authenticated } from './authenticated';
+import { AuthContext } from './auth';
 
 export function Gallery() {
-  return (
-    <DefaultLayout>
-    <main id="gallery">
-        <h2> Gallery </h2>
-        <ul>
-            <li>
-                <h3 className="date"> December 20th, 2025</h3>
-                <img className="gallery-image" src="images/ducksample.jpg" width="200px"></img>
-                <div className="prompt">Prompt: Pond </div>
-            </li>
-        </ul>
-        <button className="display-more btn btn-outline-primary my-button"> Show More</button>
-        <div className="display-logged-out">
-            Please log in to save and view your gallery
-        </div>
-        <div id="no-more-pieces">
-            End Of List
-        </div>
-        <div id="no-total-pieces"> 
-            Try the drawdle for today to add your first piece to your gallery!
-        </div>
-    </main>
-    </DefaultLayout>
-  );
+    const {isLoggedIn} = useContext(AuthContext);
+    return (
+        <DefaultLayout>
+        <main id="gallery">
+            <h2> Gallery </h2>
+            <ul>
+                <li>
+                    <h3 className="date"> December 20th, 2025</h3>
+                    <img className="gallery-image" src="images/ducksample.jpg" width="200px"></img>
+                    <div className="prompt">Prompt: Pond </div>
+                </li>
+            </ul>
+            <button className="display-more btn btn-outline-primary my-button"> Show More</button>
+            <div className="display-logged-out">
+                Please log in to save and view your gallery
+            </div>
+            <div id="no-more-pieces">
+                End Of List
+            </div>
+            <div id="no-total-pieces"> 
+                Try the drawdle for today to add your first piece to your gallery!
+            </div>
+        </main>
+        </DefaultLayout>
+    );
 }
