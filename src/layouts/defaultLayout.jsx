7fc74e6e6e82b9c3cda1a3a1b31f4a74447d1b05
adam-clark-git <adam.b.clark@icloud.com>
2026-02-23 19:23:14 -0700

    Simplifies design to have login in top-right corner on desktop and it's own page on mobile.

--- src/layouts/defaultLayout.jsx
+++ src/layouts/defaultLayout.jsx
@@ -4,6 +4,7 @@ import {NavLink} from 'react-router-dom';
 import { Authenticated } from '../login/authenticated';
 import { AuthState } from '../login/authState';
 import { AuthContext } from '../login/auth';
+import { Unauthenticated } from '../login/unauthenticated';
 
 export function DefaultLayout ({ children }) {
     const {isLoggedIn, userName} = useContext(AuthContext);
@@ -19,21 +20,20 @@ export function DefaultLayout ({ children }) {
                     <hr></hr>
                     <NavLink to='/gallery' className="playwrite-nz-basic-reg nav-item no-decoration">Gallery</NavLink>
                 </nav>
-                {isLoggedIn === AuthState.Unknown && <h1>Login Loading...</h1>}
-                {isLoggedIn === AuthState.Unauthenticated && (
-                    <form>
-                        <p className="hide-vertical"> Create an account to store and share your art with others! </p>
-                        <NavLink className="btn btn-outline-primary my-button" role="button" to='/login'> Log In </NavLink>
-                    </form>
-                )}
-                {isLoggedIn === AuthState.Authenticated && (
-                    <Authenticated/>
-                )}
-                
+                <div className="log-in-container">
+                    {isLoggedIn === AuthState.Unknown && <h1>Login Loading...</h1>}
+                    {isLoggedIn === AuthState.Unauthenticated && (
+                        <Unauthenticated/>
+                    )}
+                    {isLoggedIn === AuthState.Authenticated && (
+                        <Authenticated/>
+                    )}
+                </div>
             </header>
             {children}
             <footer>
-                <NavLink className="no-decoration back-up-login btn btn-outline-primary my-button" to='/login'> Log In </NavLink>
+                <NavLink className="no-decoration back-up-login btn btn-outline-primary my-button" to='/login'> Profile </NavLink>
+                
                 <a className="no-decoration" id="githublink" href="https://github.com/adam-clark-git/web-startup"> Adam Clark Github</a>
             </footer>
         </div>
