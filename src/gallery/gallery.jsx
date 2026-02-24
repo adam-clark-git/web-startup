e536565d68eec9a50573b5c41d0b1ff5642a8a67
adam-clark-git <adam.b.clark@icloud.com>
2026-02-24 09:57:11 -0700

    displayArt works a little better

--- src/gallery/gallery.jsx
+++ src/gallery/gallery.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { Button } from "react-bootstrap";
 import { DefaultLayout } from '../layouts/defaultLayout';
 import { AuthState } from '../login/authState';
 import { AuthContext } from '../login/auth';
@@ -7,16 +8,28 @@ import { saveData, loadData } from "./dataService";
 export function Gallery() {
     const {isLoggedIn} = useContext(AuthContext);
     const [galleryItems, setGalleryItems] = useState([]);
+    const [itemsShown, setItemsShown] = useState(5);
     useEffect(() => {
         async function fetchData() {
             setGalleryItems(await loadData("gallery"));
         }
         fetchData();
     }, []);
-    const displayArt = (start) => {
+    function handleNewItems() {
+        setItemsShown(itemsShown+5);
+    }
+    const displayArt = () => {
+        if (galleryItems.length === 0)
+        {
+            return (
+                <div> 
+                    Try the drawdle for today to add your first piece to your gallery!
+                </div>
+            );
+        }
         return (
             <>
-            {galleryItems.slice(start, start+5).map((artPiece, index) => (
+            {galleryItems.slice(0, itemsShown).map((artPiece, index) => (
                 <React.Fragment key={index}>
                     <li>
                         <h3 className="date"> {artPiece.date}</h3>
@@ -34,15 +47,13 @@ export function Gallery() {
                 <main id="gallery">
                 <h2> Gallery </h2>
                 <ul>
-                    {displayArt(0)}
+                    {displayArt()}
                 </ul>
-                <button className="display-more btn btn-outline-primary my-button"> Show More</button>
-                <div id="no-more-pieces">
-                    End Of List
-                </div>
-                <div id="no-total-pieces"> 
-                    Try the drawdle for today to add your first piece to your gallery!
-                </div>
+                {itemsShown < galleryItems.length && (
+                    <Button className="outline-primary my-button" onClick={() => handleNewItems()}> Show More</Button>
+                )}
+                
+                
                 </main>
             )}
             {isLoggedIn === AuthState.Unauthenticated && (
