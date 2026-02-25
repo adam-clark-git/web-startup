5a83dea69af62a1790b09cc43ddd489d46bd5366
adam-clark-git <adam.b.clark@icloud.com>
2026-02-25 10:32:36 -0700

    created basic pages for hosting, join, and serverInteraction

---
+++ src/multiplayer/host.jsx
@@ -0,0 +1,21 @@
+export function Host()
+{
+    const gameID = 1234;
+    return (
+        <div>
+            <h2> Hosting</h2>
+            <p> Game ID: {gameID}</p>
+            <ul>
+                <li>
+                    <p>username1</p>
+                    <button className="kick btn btn-outline-primary my-button"> Kick</button>
+                </li>
+                <li>
+                    <p>username2</p>
+                    <button className="kick btn btn-outline-primary my-button"> Kick</button>
+                </li>
+            </ul>
+            <Link id="start-multiplayer" to="/game" className="btn btn-primary my-button" role="button"> Start the Game</Link>
+        </div>
+    );
+}
---
+++ src/multiplayer/join.jsx
@@ -0,0 +1,8 @@
+export function Join()
+{
+    return (
+        <div>
+            <p> You have joined a game! Please wait until the host declares it ready.</p>
+        </div>
+    );
+}
