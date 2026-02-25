acf9b09a3c07e45c2643d9260bbb31ce7acd90ed
adam-clark-git <adam.b.clark@icloud.com>
2026-02-25 10:32:08 -0700

    Multiplayer changed to show pages based on permissions

--- src/multiplayer/multiplayer.jsx
+++ src/multiplayer/multiplayer.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { DefaultLayout } from '../layouts/defaultLayout';
+import Button from 'react-bootstrap/Button';
+import { AuthState } from '../login/authState';
+import { AuthContext } from '../login/auth';
+import { Host } from "./host.jsx";
+import { Join } from "./join.jsx"; 
 
 export function Multiplayer() {
-  return (
-    <DefaultLayout>
-    <main id="drawfriends">
-        <h2> Multiplayer </h2>
-        <div className="display-logged-out">
-            Please log in to play with friends.
-        </div>
-        <div>
-            <form>
-                <label for="text">Input Code: </label>
-                <input type="text" id="code-input" name="varCode"/>
-                <button id="join-lobby" className="btn btn-outline-primary my-button"> Join </button>
-            </form>
-            <br></br>
-            <p> Or </p>
-            <button id="create-lobby" className="btn btn-outline-primary my-button"> Create Lobby</button>
-            <ul>
-                <li>
-                    <p>username1</p>
-                    <button className="kick btn btn-outline-primary my-button"> Kick</button>
-                </li>
-                <li>
-                    <p>username2</p>
-                    <button className="kick btn btn-outline-primary my-button"> Kick</button>
-                </li>
-            </ul>
-            <Link id="start-multiplayer" to="/game" className="btn btn-primary my-button" role="button"> Start the Game</Link>
-        </div>
-    </main>
-    </DefaultLayout>
-  );
+    const {isLoggedIn} = useContext(AuthContext);
+    return (
+        <DefaultLayout>
+        <main id="drawfriends">
+            <h2> Multiplayer </h2>
+            {isLoggedIn === AuthState.Unauthenticated && (
+                <div className="display-logged-out">
+                    Please log in to play with friends.
+                </div>
+            )}
+            {isLoggedIn === AuthState.Authenticated && (
+                <div>
+                    <form>
+                        <label for="text">Input Code: </label>
+                        <input type="text" id="code-input" name="varCode"/>
+                        <Button id="join-lobby" className="outline-primary my-button"> Join </Button>
+                    </form>
+                    <br></br>
+                    <p> Or </p>
+                    <Button id="create-lobby" className="outline-primary my-button"> Create Lobby</Button>
+                </div>
+            )}
+            
+            
+            
+        </main>
+        </DefaultLayout>
+    );
 }
