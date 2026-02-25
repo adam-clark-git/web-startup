fe5b5792b67b70418d736431cffce37ec35fb976
adam-clark-git <adam.b.clark@icloud.com>
2026-02-25 00:18:27 -0700

    Now changes the button look depending on local storage

--- src/main/main.jsx
+++ src/main/main.jsx
@@ -2,16 +2,43 @@ import React, { useEffect, useState }from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { DefaultLayout } from '../layouts/defaultLayout';
+import { loadData } from "../gallery/dataService"
 
 
 export function Main() {
-    const [lastPlayed] = useState(localStorage.getItem("lastPlayed")) || [];
+    const [lastPlayed] = useState(() => {
+        const item = localStorage.getItem("localSaved");
+        return item ? JSON.parse(item) : null;
+    });
+    const [currentDate] = useState(new Date().toLocaleDateString()) || [];
     const [showModal, setShowModal] = useState(false);
+    function DisplayDrawdle()
+    {
+        let words = "Play Daily Drawdle";
+        console.log("Displayed")
+        if (lastPlayed !== null) {
+            console.log("Sees lastPlayed");
+            console.log(lastPlayed.finished);
+            console.log(lastPlayed.date);
+            if (lastPlayed.finished) {
+                console.log("Should display results");
+                words = "Results";
+            }
+            else if (lastPlayed.date === currentDate) {
+                console.log("Should display continue");
+                words = "Continue Daily Drawdle";
+            }
+        }
+        else {
+            console.log("Doesn't see it");
+        }
+        return <Link id="main-play" className="btn btn-primary my-button" role="button" to='game'> {words} </Link>;
+    }
   return (
     <DefaultLayout>
     <main className="frontpage">
         <Button className="btn btn-info my-button" onClick={() => setShowModal(true)}> How To Play </Button>
-        <Link id="main-play" className="btn btn-primary my-button" role="button" to='game'> Play Daily Drawdle </Link>
+        <DisplayDrawdle/>
         <Link className="btn btn-secondary my-button" role="button" to='multiplayer'> Multiplayer </Link>
         
         <Modal show={showModal} onHide={() => setShowModal(false)}>
