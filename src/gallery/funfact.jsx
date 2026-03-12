7712192d72f002fd4252821c5e6e18437db89339
adam-clark-git <adam.b.clark@icloud.com>
2026-03-12 15:46:58 -0600

    created funfact.jsx to meet third party api requirement

---
+++ src/gallery/funfact.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+export default function FactOfTheDay() {
+
+
+
+    return (
+        <div>
+            <span> Fun Fact! </span>
+            
+        </div>
+    )
+}
